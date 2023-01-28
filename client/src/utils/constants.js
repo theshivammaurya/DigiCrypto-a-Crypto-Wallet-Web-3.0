cd72b4bc86ebd9b0699cb71f60a8827bdda1609b
Shivam Kumar Maurya <shivam05046@gmail.com>
2023-01-28 13:36:12 +0530

    initial commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from './Transactions.json';
+
+
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x49c4d8ABa1010A3DEe5B18eEE4914C543Cb165BD";
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,17 @@
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.17',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/9t_zUUPufRp0iFau-YU6JNQsMGVRmYZS',
+      accounts: ['aecef2fb19f0c1f212edcab37e392136276170675b03113923ec5bdf78774f62'],
+    },
+  },
+};
+
+
+//https://eth-goerli.g.alchemy.com/v2/9t_zUUPufRp0iFau-YU6JNQsMGVRmYZS
+
+//0x49c4d8ABa1010A3DEe5B18eEE4914C543Cb165BD
